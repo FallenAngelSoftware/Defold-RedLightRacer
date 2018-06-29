5fe505eec65a658531753348b83c8ab42d61adce
FallenAngelSoftware <Admin@FallenAngelSoftware.com>
2018-06-29 11:47:21 -0400

    0.0.2

--- data/objects/Screen-2-Title.go
+++ data/objects/Screen-2-Title.go
@@ -103,6 +103,21 @@ components {
     w: 1.0
   }
 }
+components {
+  id: "level_select"
+  component: "/data/gui/level_select.gui"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
 embedded_components {
   id: "RLR-Logo"
   type: "sprite"
