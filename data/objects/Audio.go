452e76a15c78985344c66f000ad4c88bbd487e3c
FallenAngelSoftware <Admin@FallenAngelSoftware.com>
2018-07-02 03:11:29 -0400

    0.0.2

---
+++ data/objects/Audio.go
@@ -0,0 +1,80 @@
+embedded_components {
+  id: "Title-BGM"
+  type: "sound"
+  data: "sound: \"/data/audio/Title-BGM.ogg\"\n"
+  "looping: 1\n"
+  "group: \"master\"\n"
+  "gain: 1.0\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "MenuClick"
+  type: "sound"
+  data: "sound: \"/data/audio/MenuClick.ogg\"\n"
+  "looping: 0\n"
+  "group: \"master\"\n"
+  "gain: 1.0\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "MenuMove"
+  type: "sound"
+  data: "sound: \"/data/audio/MenuMove.ogg\"\n"
+  "looping: 0\n"
+  "group: \"master\"\n"
+  "gain: 1.0\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "RedLightRacer"
+  type: "sound"
+  data: "sound: \"/data/audio/RedLightRacer.ogg\"\n"
+  "looping: 0\n"
+  "group: \"master\"\n"
+  "gain: 1.0\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
