4df6554c4adb42e81fd28479e9af33cf81ad8684
TheFallenAngel <Admin@FallenAngelSoftware.com>
2019-02-10 09:09:54 -0500

    0.5.0.7

--- data/modules/PlayMusic.lua
+++ data/modules/PlayMusic.lua
@@ -19,6 +19,7 @@ function MTWO.PlayMusic(music)
 	elseif (music == 9) then msg.post("level:/Audio#WinTeen-BGM", "play_sound", {delay = 0, gain = 1})
 	elseif (music == 10) then msg.post("level:/Audio#WinAdult-BGM", "play_sound", {delay = 0, gain = 1})
 	elseif (music == 11) then msg.post("level:/Audio#MsPalser-BGM", "play_sound", {delay = 0, gain = 1})
+	CurrentMusicPlaying = music
 	end
 end
 
