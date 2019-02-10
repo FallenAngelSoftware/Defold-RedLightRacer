9400e693487eebd0873d13c5fbbd3f9e7c38f11c
TheFallenAngel <Admin@FallenAngelSoftware.com>
2019-02-10 12:58:49 -0500

    0.5.0.9

--- data/objects/Screen-9-NewHighScore.go
+++ data/objects/Screen-9-NewHighScore.go
@@ -58,6 +58,21 @@ components {
     w: 1.0
   }
 }
+components {
+  id: "NewHighScoreName"
+  component: "/data/labels/NewHighScoreName.label"
+  position {
+    x: 180.0
+    y: 520.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
 embedded_components {
   id: "BG"
   type: "sprite"
@@ -109,6 +124,80 @@ embedded_components {
   position {
     x: 180.0
     y: 55.0
+    z: 0.25
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "EnterYourName"
+  type: "label"
+  data: "size {\n"
+  "  x: 128.0\n"
+  "  y: 32.0\n"
+  "  z: 0.0\n"
+  "  w: 0.0\n"
+  "}\n"
+  "scale {\n"
+  "  x: 1.0\n"
+  "  y: 1.0\n"
+  "  z: 1.0\n"
+  "  w: 0.0\n"
+  "}\n"
+  "color {\n"
+  "  x: 1.0\n"
+  "  y: 1.0\n"
+  "  z: 1.0\n"
+  "  w: 1.0\n"
+  "}\n"
+  "outline {\n"
+  "  x: 0.0\n"
+  "  y: 0.0\n"
+  "  z: 0.0\n"
+  "  w: 1.0\n"
+  "}\n"
+  "shadow {\n"
+  "  x: 0.0\n"
+  "  y: 0.0\n"
+  "  z: 0.0\n"
+  "  w: 1.0\n"
+  "}\n"
+  "leading: 1.0\n"
+  "tracking: 0.0\n"
+  "pivot: PIVOT_CENTER\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  "line_break: false\n"
+  "text: \"ENTER YOUR NAME:\"\n"
+  "font: \"/data/fonts/GameFont.font\"\n"
+  "material: \"/builtins/fonts/label.material\"\n"
+  ""
+  position {
+    x: 180.0
+    y: 569.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "LineMiddle"
+  type: "sprite"
+  data: "tile_set: \"/data/atlases/ScreenLine.atlas\"\n"
+  "default_animation: \"ScreenLine\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  ""
+  position {
+    x: 180.0
+    y: 483.0
     z: 0.25
   }
   rotation {
