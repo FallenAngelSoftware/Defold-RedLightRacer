776276555b522996f6a8830efab0a7e80d1a389c
TheFallenAngel <Admin@FallenAngelSoftware.com>
2019-02-10 12:05:42 -0500

    0.5.0.8

--- data/objects/Screen-9-NewHighScore.go
+++ data/objects/Screen-9-NewHighScore.go
@@ -43,6 +43,21 @@ components {
     w: 1.0
   }
 }
+components {
+  id: "ScreenHeader"
+  component: "/data/labels/ScreenHeader.label"
+  position {
+    x: 180.0
+    y: 625.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
 embedded_components {
   id: "BG"
   type: "sprite"
@@ -55,6 +70,46 @@ embedded_components {
     x: 180.0
     y: 320.0
     z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "LineTop"
+  type: "sprite"
+  data: "tile_set: \"/data/atlases/ScreenLine.atlas\"\n"
+  "default_animation: \"ScreenLine\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  ""
+  position {
+    x: 180.0
+    y: 607.0
+    z: 0.25
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "LineBottom"
+  type: "sprite"
+  data: "tile_set: \"/data/atlases/ScreenLine.atlas\"\n"
+  "default_animation: \"ScreenLine\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  ""
+  position {
+    x: 180.0
+    y: 55.0
+    z: 0.25
   }
   rotation {
     x: 0.0
