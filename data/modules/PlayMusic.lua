9b88f4b7d0fe03dcb7c011b81877379fb2f9ff56
TheFallenAngel <Admin@FallenAngelSoftware.com>
2019-02-10 16:14:18 -0500

    0.5.0.13

--- data/modules/PlayMusic.lua
+++ data/modules/PlayMusic.lua
@@ -7,18 +7,18 @@ local MTWO = {}
 
 function MTWO.PlayMusic(music)
 
-	if (music == 0) then msg.post("level:/Audio#Title-BGM", "play_sound", {delay = 0, gain = 1})
-	elseif (music == 1) then msg.post("level:/Audio#Cutscenes-BGM", "play_sound", {delay = 0, gain = 1})
-	elseif (music == 2) then msg.post("level:/Audio#Level12-BGM", "play_sound", {delay = 0, gain = 1})
-	elseif (music == 3) then msg.post("level:/Audio#Level34-BGM", "play_sound", {delay = 0, gain = 1})
-	elseif (music == 4) then msg.post("level:/Audio#Level56-BGM", "play_sound", {delay = 0, gain = 1})
-	elseif (music == 5) then msg.post("level:/Audio#Level78-BGM", "play_sound", {delay = 0, gain = 1})
-	elseif (music == 6) then msg.post("level:/Audio#Level910-BGM", "play_sound", {delay = 0, gain = 1})
-	elseif (music == 7) then msg.post("level:/Audio#NewHighScore-BGM", "play_sound", {delay = 0, gain = 1})
-	elseif (music == 8) then msg.post("level:/Audio#WinChild-BGM", "play_sound", {delay = 0, gain = 1})
-	elseif (music == 9) then msg.post("level:/Audio#WinTeen-BGM", "play_sound", {delay = 0, gain = 1})
-	elseif (music == 10) then msg.post("level:/Audio#WinAdult-BGM", "play_sound", {delay = 0, gain = 1})
-	elseif (music == 11) then msg.post("level:/Audio#MsPalser-BGM", "play_sound", {delay = 0, gain = 1})
+	if (music == 0) then msg.post("level:/Audio#Title-BGM", "play_sound", {delay = 0, gain = MusicVolume})
+	elseif (music == 1) then msg.post("level:/Audio#Cutscenes-BGM", "play_sound", {delay = 0, gain = MusicVolume})
+	elseif (music == 2) then msg.post("level:/Audio#Level12-BGM", "play_sound", {delay = 0, gain = MusicVolume})
+	elseif (music == 3) then msg.post("level:/Audio#Level34-BGM", "play_sound", {delay = 0, gain = MusicVolume})
+	elseif (music == 4) then msg.post("level:/Audio#Level56-BGM", "play_sound", {delay = 0, gain = MusicVolume})
+	elseif (music == 5) then msg.post("level:/Audio#Level78-BGM", "play_sound", {delay = 0, gain = MusicVolume})
+	elseif (music == 6) then msg.post("level:/Audio#Level910-BGM", "play_sound", {delay = 0, gain = MusicVolume})
+	elseif (music == 7) then msg.post("level:/Audio#NewHighScore-BGM", "play_sound", {delay = 0, gain = MusicVolume})
+	elseif (music == 8) then msg.post("level:/Audio#WinChild-BGM", "play_sound", {delay = 0, gain = MusicVolume})
+	elseif (music == 9) then msg.post("level:/Audio#WinTeen-BGM", "play_sound", {delay = 0, gain = MusicVolume})
+	elseif (music == 10) then msg.post("level:/Audio#WinAdult-BGM", "play_sound", {delay = 0, gain = MusicVolume})
+	elseif (music == 11) then msg.post("level:/Audio#MsPalser-BGM", "play_sound", {delay = 0, gain = MusicVolume})
 	CurrentMusicPlaying = music
 	end
 end
