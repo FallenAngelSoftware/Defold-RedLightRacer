e9bbade4b0159ba99b6056c6be34d38a9eb40f3f
TheFallenAngel <Admin@FallenAngelSoftware.com>
2019-02-14 11:50:22 -0500

    0.5.0.29

--- data/objects/Screen-8-Playing.go
+++ data/objects/Screen-8-Playing.go
@@ -43,6 +43,21 @@ components {
     w: 1.0
   }
 }
+components {
+  id: "Score"
+  component: "/data/labels/Score.label"
+  position {
+    x: 180.0
+    y: 625.0
+    z: 1.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
 embedded_components {
   id: "Street"
   type: "sprite"
@@ -55,6 +70,26 @@ embedded_components {
     x: 180.0
     y: 320.0
     z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "Player"
+  type: "sprite"
+  data: "tile_set: \"/data/atlases/Car.atlas\"\n"
+  "default_animation: \"Car\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  ""
+  position {
+    x: 180.0
+    y: 105.0
+    z: 1.0
   }
   rotation {
     x: 0.0
