54ceea5e24db18bcd30177a93a073780152e715d
TheFallenAngel <Admin@FallenAngelSoftware.com>
2019-02-14 10:11:47 -0500

    0.5.0.28

---
+++ data/objects/Screen-8-Playing.go
@@ -0,0 +1,65 @@
+components {
+  id: "MouseXY"
+  component: "/data/labels/MouseXY.label"
+  position {
+    x: 5.0
+    y: 630.0
+    z: 0.1
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+components {
+  id: "Screen-8-Playing-GUI"
+  component: "/data/gui/Screen-8-Playing-GUI.gui"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+components {
+  id: "Screen-8"
+  component: "/Screen-8-Playing/Screen-8.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "Street"
+  type: "sprite"
+  data: "tile_set: \"/data/atlases/Street.atlas\"\n"
+  "default_animation: \"Street\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  ""
+  position {
+    x: 180.0
+    y: 320.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
