8ab74f6da6f164a744bff93f27ea4594f638d4cd
TheFallenAngel <Admin@FallenAngelSoftware.com>
2019-02-18 09:04:17 -0500

    0.9.9.0

--- data/objects/Screen-2-Title.go
+++ data/objects/Screen-2-Title.go
@@ -159,26 +159,6 @@ embedded_components {
   }
 }
 embedded_components {
-  id: "GetItOnGooglePlay"
-  type: "sprite"
-  data: "tile_set: \"/data/atlases/GetItOnGooglePlay.atlas\"\n"
-  "default_animation: \"GooglePlayLogo\"\n"
-  "material: \"/builtins/materials/sprite.material\"\n"
-  "blend_mode: BLEND_MODE_ALPHA\n"
-  ""
-  position {
-    x: 180.0
-    y: 81.0
-    z: 0.5
-  }
-  rotation {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-    w: 1.0
-  }
-}
-embedded_components {
   id: "ScreenLine1"
   type: "sprite"
   data: "tile_set: \"/data/atlases/ScreenLine.atlas\"\n"
